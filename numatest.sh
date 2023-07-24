46435f16123febb6935a3efcd491e16e1c02a7fc
Tan Xiaofan <xiaofan@iscas.ac.cn>
2023-07-24 14:08:09 +0800

    Fix numatest.sh
    Signed-off-by: Tan Xiaofan <xiaofan@iscas.ac.cn>

--- numatest.sh
+++ numatest.sh
@@ -5,13 +5,17 @@ set -e
 node_count=`lscpu | fgrep 'NUMA node(s)' | grep -Eo '[0-9]+$'`
 if [ -z "$node_count" ]; then
     echo "Error: cannot get NUMA node count"
+    exit 1
+fi
+
+if [ -z "$*" ]; then
+    echo "Usage: $0 <command>"
+    exit 1
 fi
 
 for run_node in `seq 0 $((node_count-1))`; do
     for mem_node in `seq 0 $((node_count-1))`; do
-        if [ $run_node -eq $mem_node ]; then
-            echo "Run on node $run_node, allocate on node $mem_node"
-            numactl --cpunodebind=$run_node --membind=$mem_node "$@"
-        fi
+        echo "Run on node $run_node, allocate on node $mem_node"
+        numactl --cpunodebind=$run_node --membind=$mem_node "$@"
     done
 done
